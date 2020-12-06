1245df51cd95375ac2ea69cb373be674ac489462
Arvin Fernandez <arvinf07@gmail.com>
2020-12-06 14:18:58 -0500

    commit tree 2bfbee0dfe3258a0ca09179d79252c80f7bf12d4

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class PetsController < ApplicationController
 
   get '/pets' do
@@ -6,12 +8,24 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    if params['pet'].empty?
+      redirect '/pets/new'
+    end
+    @pet = Pet.create(params['pet'])
+    @pet.owner = Owner.create(name: params[:owner_name]) if !params[:owner_name].empty?
+    @pet.save
     redirect to "pets/#{@pet.id}"
+  end
+  
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
   end
 
   get '/pets/:id' do 
@@ -20,7 +34,10 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
-
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    @pet.owner = Owner.create(name: params[:owner][:name]) if !params[:owner][:name].empty?
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
